49a222c421ae17fcf308e308c063b17af4baf73b
Michael Hillmann <hillmann@embedded-office.de>
2022-07-27 17:30:17 +0200

    add cia301 unit test for dynamic PDO mapping

--- canopen/source/cia301/co_pdo_map.c
+++ canopen/source/cia301/co_pdo_map.c
@@ -37,12 +37,13 @@
 static uint32_t COTPdoMapSize (struct CO_OBJ_T *obj, struct CO_NODE_T *node, uint32_t width);
 static CO_ERR   COTPdoMapRead (struct CO_OBJ_T *obj, struct CO_NODE_T *node, void *buffer, uint32_t size);
 static CO_ERR   COTPdoMapWrite(struct CO_OBJ_T *obj, struct CO_NODE_T *node, void *buffer, uint32_t size);
+static CO_ERR   COTPdoMapInit (struct CO_OBJ_T *obj, struct CO_NODE_T *node);
 
 /******************************************************************************
 * PUBLIC GLOBALS
 ******************************************************************************/
 
-const CO_OBJ_TYPE COTPdoMap = { COTPdoMapSize, 0, COTPdoMapRead, COTPdoMapWrite, 0 };
+const CO_OBJ_TYPE COTPdoMap = { COTPdoMapSize, COTPdoMapInit, COTPdoMapRead, COTPdoMapWrite, 0 };
 
 /******************************************************************************
 * PRIVATE TYPE FUNCTIONS
@@ -73,55 +74,70 @@ static CO_ERR COTPdoMapWrite(struct CO_OBJ_T *obj, struct CO_NODE_T *node, void 
     uint16_t  pcomidx;
     uint8_t   mapn;
 
-    UNUSED(size);
     ASSERT_PTR_ERR(obj, CO_ERR_BAD_ARG);
     ASSERT_PTR_ERR(buffer, CO_ERR_BAD_ARG);
 
-    if (CO_GET_SUB(obj->Key) < 1) {
-        return (CO_ERR_TPDO_MAP_OBJ);
-    }
-
     cod     = &node->Dict;
     pmapidx = CO_GET_IDX(obj->Key);
-    if ((pmapidx >= COT_OBJECT_RPDO) && (pmapidx <= COT_OBJECT_RPDO + COT_OBJECT_NUM)) {
-    } else if ((pmapidx >= COT_OBJECT_TPDO) && (pmapidx <= COT_OBJECT_TPDO + COT_OBJECT_NUM)) {
-    } else {
-        return (CO_ERR_TPDO_MAP_OBJ);
-    }
-
     pcomidx = pmapidx - 0x200;
     map     = *(uint32_t*)buffer;
+
+    /* check that PDO is inactive */
     (void)CODictRdLong(cod, CO_DEV(pcomidx, 1), &id);
     if ((id & CO_TPDO_COBID_OFF) == 0) {
-        result = CO_ERR_OBJ_ACC;
-    } else {
-        (void)CODictRdByte(cod, CO_DEV(pmapidx, 0), &mapn);
-        if (mapn != 0) {
-            return (CO_ERR_OBJ_ACC);
-        }
+        return (CO_ERR_OBJ_ACC);
+    }
 
-        objm = CODictFind(cod, map);
-        if (objm == (CO_OBJ*)0) {
-            return (CO_ERR_OBJ_MAP_TYPE);
-        }
+    /* check number of PDO mapping entries */
+    (void)CODictRdByte(cod, CO_DEV(pmapidx, 0), &mapn);
+    if (mapn != 0) {
+        return (CO_ERR_OBJ_ACC);
+    }
 
-        maps = CO_IS_PDOMAP(objm->Key);
+    /* check that linked object exists */
+    objm = CODictFind(cod, map);
+    if (objm == (CO_OBJ*)0) {
+        return (CO_ERR_OBJ_MAP_TYPE);
+    }
+
+    /* check that linked object is PDO mappable */
+    maps = CO_IS_PDOMAP(objm->Key);
+    if (maps == 0) {
+        return (CO_ERR_OBJ_MAP_TYPE);
+    }
+
+    /* check CAN access permissions for linked object */
+    if ((pmapidx >= COT_OBJECT_RPDO) && (pmapidx <= COT_OBJECT_RPDO + COT_OBJECT_NUM)) {
+        maps = CO_IS_WRITE(objm->Key);
         if (maps == 0) {
             return (CO_ERR_OBJ_MAP_TYPE);
         }
-
-        if ((pmapidx >= COT_OBJECT_RPDO) && (pmapidx <= COT_OBJECT_RPDO + COT_OBJECT_NUM)) {
-            maps = CO_IS_WRITE(objm->Key);
-            if (maps == 0) {
-                return (CO_ERR_OBJ_MAP_TYPE);
-            }
-        } else if ((pmapidx >= COT_OBJECT_TPDO) && (pmapidx <= COT_OBJECT_TPDO + COT_OBJECT_NUM)) {
-            maps = CO_IS_READ(objm->Key);
-            if (maps == 0) {
-                return (CO_ERR_OBJ_MAP_TYPE);
-            }
+    } else if ((pmapidx >= COT_OBJECT_TPDO) && (pmapidx <= COT_OBJECT_TPDO + COT_OBJECT_NUM)) {
+        maps = CO_IS_READ(objm->Key);
+        if (maps == 0) {
+            return (CO_ERR_OBJ_MAP_TYPE);
         }
-        result = uint32->Write(obj, node, &map, sizeof(map));
+    }
+
+    /* ok, write new PDO mapping */
+    result = uint32->Write(obj, node, &map, sizeof(map));
+    return (result);
+}
+
+static CO_ERR COTPdoMapInit(struct CO_OBJ_T *obj, struct CO_NODE_T *node)
+{
+    CO_ERR result = CO_ERR_TYPE_INIT;
+
+    if ((CO_GET_IDX(obj->Key) >= COT_OBJECT_RPDO) &&
+        (CO_GET_IDX(obj->Key) <= COT_OBJECT_RPDO + COT_OBJECT_NUM)) {
+        if (CO_GET_SUB(obj->Key) > 0) {
+            result = CO_ERR_NONE;
+        }
+    } else if ((CO_GET_IDX(obj->Key) >= COT_OBJECT_TPDO) &&
+               (CO_GET_IDX(obj->Key) <= COT_OBJECT_TPDO + COT_OBJECT_NUM)) {
+        if (CO_GET_SUB(obj->Key) > 0) {
+            result = CO_ERR_NONE;
+        }
     }
     return (result);
 }
